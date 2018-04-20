c689dcd19fffcc84d6eae85c9a7894f4cba635c7
Aleksandr Danshyn <dalazx@gmail.com>
2018-04-20 11:00:43 +0300

    Added aiohttp and related stuff

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ from setuptools import setup, find_packages
 
 
 install_requires = (
-    'aiofiles==0.3.2'
+    'aiofiles==0.3.2',
+    'cchardet==2.1.1',
+    'aiodns==1.1.1',
+    'aiohttp==3.1.3',
+    # TODO (A Danshyn 04/20/18): add uvloop at some point
 )
 
 setup(
