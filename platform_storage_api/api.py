c5ca1e8ec9363fd1f8391785ff29346fafdb3dda
Aleksandr Danshyn <dalazx@gmail.com>
2018-04-23 15:41:41 +0300

    Integrated Storage into StorageHandler

--- platform_storage_api/api.py
+++ platform_storage_api/api.py
@@ -1,4 +1,9 @@
+from pathlib import PurePath
+
 import aiohttp.web
+
+from .fs.local import FileSystem, copy_streams
+from .storage import Storage
 
 
 class ApiHandler:
@@ -12,26 +17,40 @@ class ApiHandler:
 
 
 class StorageHandler:
+    def __init__(self, fs):
+        self._fs = fs
+        # TODO (A Danshyn 04/23/18): drop the hardcoded path
+        self._storage = Storage(fs, '/tmp/np_storage')
+
     def register(self, app):
         app.add_routes((
-            aiohttp.web.get(r'/{path:\.*}', self.handle_put),
+            # TODO (A Danshyn 04/23/18): add some unit test for this
+            aiohttp.web.put(r'/{path:.*}', self.handle_put),
         ))
 
-    def handle_put(self, request):
-        pass
+    def _get_fs_path_from_request(self, request):
+        return PurePath('/', request.match_info['path'])
+
+    async def handle_put(self, request):
+        storage_path = self._get_fs_path_from_request(request)
+        await self._storage.store(request.content, storage_path)
+        return aiohttp.web.Response(status=201)
+
+    async def handle_get(self, request):
+        # TODO (A Danshyn 04/23/18): check if exists (likely in some
+        # middleware)
+        return aiohttp.web.Response(status=200)
 
 
-async def create_app():
+async def create_app(fs: FileSystem):
     app = aiohttp.web.Application()
-
-    storage_app = aiohttp.web.Application()
 
     api_v1_app = aiohttp.web.Application()
     api_v1_handler = ApiHandler()
     api_v1_handler.register(api_v1_app)
 
     storage_app = aiohttp.web.Application()
-    storage_handler = StorageHandler()
+    storage_handler = StorageHandler(fs=fs)
     storage_handler.register(storage_app)
 
     api_v1_app.add_subapp('/storage', storage_app)
