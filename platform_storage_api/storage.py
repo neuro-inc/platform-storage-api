f65dc4e627ec32112111886c1d363b2bf31b7c2e
Aleksandr Danshyn <dalazx@gmail.com>
2018-04-23 15:41:14 +0300

    Added initial Storage

---
+++ platform_storage_api/storage.py
@@ -0,0 +1,23 @@
+from pathlib import PurePath
+from typing import Union
+
+from .fs.local import FileSystem, copy_streams
+
+
+class Storage:
+    def __init__(
+            self, fs: FileSystem, base_path: Union[PurePath, str]) -> None:
+        self._fs = fs
+        self._base_path = PurePath(base_path)
+
+        # TODO (A Danshyn 04/23/18): implement StoragePathResolver
+
+    def _resolve_real_path(self, path: PurePath):
+        # TODO: (A Danshyn 04/23/18): validate paths
+        return PurePath(self._base_path, path.relative_to('/'))
+
+    async def store(self, outstream, path):
+        real_path = self._resolve_real_path(path)
+        await self._fs.mkdir(real_path.parent)
+        async with self._fs.open(real_path, 'wb') as f:
+            await copy_streams(outstream, f)
