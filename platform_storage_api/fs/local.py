b2ccee009dda4c8d9a4a5d73f654933fd9ae8889
Roman Skurikhin <romasku135@gmail.com>
2020-10-20 17:15:38 +0300

    Fix types for aiofiles stubs

--- platform_storage_api/fs/local.py
+++ platform_storage_api/fs/local.py
@@ -206,7 +206,14 @@ class LocalFileSystem(FileSystem):
     # Actual return type is an async version of io.FileIO
     @contextlib.asynccontextmanager
     async def open(self, path: PurePath, mode: str = "r") -> Any:
-        async with aiofiles.open(path, mode=mode, executor=self._executor) as f:
+        # typeshed have added types for aiofiles, but they aren't complete
+        # Probably this PR will fix it: https://github.com/python/typeshed/pull/4650
+        # Ignoring types for now
+        async with aiofiles.open(  # type: ignore
+            path,
+            mode=mode,
+            executor=self._executor,
+        ) as f:
             yield f
 
     def _mkdir(self, path: PurePath) -> None:
