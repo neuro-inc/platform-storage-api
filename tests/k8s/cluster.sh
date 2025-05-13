19c448b187ce69d9b86511cafac8008389c9035a
oleksandr-sandbx <olexandr.kostiuk@sandbx.co>
2025-05-13 19:42:43 +0200

    test

--- tests/k8s/cluster.sh
+++ tests/k8s/cluster.sh
@@ -11,8 +11,6 @@ function k8s::install_minikube {
     curl -Lo minikube https://storage.googleapis.com/minikube/releases/${minikube_version}/minikube-linux-amd64
     chmod +x minikube
     sudo mv minikube /usr/local/bin/
-    sudo -E minikube config set WantReportErrorPrompt false
-    sudo -E minikube config set WantNoneDriverWarning false
 }
 
 function k8s::start {
